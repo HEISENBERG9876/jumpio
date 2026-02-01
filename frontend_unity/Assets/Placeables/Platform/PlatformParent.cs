5884f01e394644460c6d4abfaa42b48d568c2488
HEISENBERG9876 <dwalecko7@gmail.com>
2026-02-01 01:36:50 +0100

    add new windows and linux builds, training unchanged

---
+++ frontend_unity/Assets/Placeables/Platform/PlatformParent.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlatformParent : MonoBehaviour
+{
+    [SerializeField] private LayerMask passengerMask;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!IsPassenger(collision))
+        {
+            Debug.Log("Not a passenger");
+            return;
+        }
+
+        Debug.Log("Setting parent");
+        collision.transform.SetParent(transform, true);
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.parent == transform)
+        {
+            collision.transform.SetParent(null, true);
+        }
+    }
+
+    private bool IsPassenger(Collision2D collision)
+    {
+        return (passengerMask.value & (1 << collision.gameObject.layer)) != 0;
+    }
+}
